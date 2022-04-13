8ee4a3bd580140b5d4ac290cb7e59c006c09d295
milanszabo2001 <94606015+milanszabo2001@users.noreply.github.com>
2022-04-13 10:33:51 +0200

    Add files via upload

---
+++ data1.js
@@ -0,0 +1,62 @@
+const kerdesek = [
+    {
+       id:1, 
+      question: "Melyik film aratta a valaha volt legnagyobb sikert a film iparban?",
+      answers: {
+        a: "Titanic",
+        b: "Avengers: End Game",
+        c: "Avatar",
+        d: "Star Wars: Az ébredő erő"
+      },
+      correctAnswer: "c",
+      image:"avatar-2.jpg"
+    },
+    {
+       id:2, 
+      question: "Az alábbi filmek közül melyikben NEM szerepelt Tom Holland?",
+      answers: {
+        a: "Pókember: Hazatérés",
+        b: "A csodálatos Pókember",
+        c: "Pókember: Idegenben",
+        d: "Pókember: Nincs hazaút"
+      },
+      correctAnswer: "b",
+      image:"andrewgarfield.jpg"
+    },
+    {
+       id:3, 
+      question: "Az alábbi személyek közül ki nyert 2018-ban Oscar díjat?",
+      answers: {
+        a: "Gary Oldman",
+        b: "Will Smith",
+        c: "Brad Pitt",
+        d: "Jason Statham"
+      },
+      correctAnswer: "a",
+      image:"garyoldman.jpg"
+    },
+    {
+        id:4, 
+       question: "Az alábbi személyek közül melyik NEM színész?",
+       answers: {
+         a: "Cary Grant",
+         b: "Paul Newman",
+         c: "Don Cheadle",
+         d: "Bobby Miller"
+       },
+       correctAnswer: "d",
+       image:"bobbymiller.jpg"
+     },
+     {
+        id:5, 
+       question: "Hány darab Batman film létezik?",
+       answers: {
+         a: "12",
+         b: "13",
+         c: "14",
+         d: "15"
+       },
+       correctAnswer: "c",
+       image:"14.png"
+     }
+  ];
